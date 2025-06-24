1714b83795927784e790ba0ebff9ba2ca5c51576
akshat-bajpai <akshat24056@iiitd.ac.in>
2025-06-24 17:06:39 +0000

    [LeetCode Sync] Runtime - 75 ms (27.80%), Memory - 47.7 MB (27.78%)

---
+++ problems/0560-subarray-sum-equals-k/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        map<int,int> mpp;
+        mpp[0]=1;
+        int sum=0;
+        int cnt=0;
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
+            if (mpp.find(sum-k)!=mpp.end()){
+                cnt+=mpp[sum-k];
+            }
+            mpp[sum]++;
+        }
+        return cnt;
+    }
+};
