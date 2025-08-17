335b52a5c024ad369439abb4712372587443e577
akshat-bajpai <akshat24056@iiitd.ac.in>
2025-08-17 06:25:01 +0000

    [LeetCode Sync] Runtime - 15 ms (27.84%), Memory - 11.1 MB (28.45%)

---
+++ problems/1460-number-of-substrings-containing-all-three-characters/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int numberOfSubstrings(string s) {
+        vector<int> lastSeen={-1,-1,-1};
+        int count=0;
+        for (int i=0;i<s.size();i++){
+            lastSeen[s[i]-'a']=i;
+
+            if (lastSeen[0]!=-1 && lastSeen[1]!=-1 && lastSeen[2]!=-1){
+                count+=min(lastSeen[0],min(lastSeen[1],lastSeen[2]))+1;
+            }
+        }
+        return count;
+    }
+};
