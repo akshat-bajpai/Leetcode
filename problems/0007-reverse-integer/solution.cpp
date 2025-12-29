071de2032c16e2c8d0eb72047e5d9b1bd412c3be
akshat-bajpai <akshat24056@iiitd.ac.in>
2025-12-29 10:37:32 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8.7 MB (21.31%)

--- problems/0007-reverse-integer/solution.cpp
+++ problems/0007-reverse-integer/solution.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
-    int reverse(int x) {
+    int reverse(int x){
+        int ans=0;
         int max=INT_MAX;
         int min=INT_MIN;
-        int num=0;
+
         while (x!=0){
-            if (num>max/10 || num<min/10) return 0;
-            num=num*10+x%10;
+            if (ans>max/10 || ans<min/10) return 0;
+            ans=ans*10+(x%10);
             x=x/10;
         }
-        return num;
+        return ans;
     }
+        
 };
