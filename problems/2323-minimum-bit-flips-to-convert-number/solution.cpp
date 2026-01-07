a2c0e6819fcdcffbb99534ff2a73e7f78004989f
akshat-bajpai <akshat24056@iiitd.ac.in>
2026-01-07 12:13:59 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8 MB (60.72%)

---
+++ problems/2323-minimum-bit-flips-to-convert-number/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minBitFlips(int start, int goal) {
+
+        int x=start^goal;
+        int c=0;
+
+        if (x==0) return 0;
+
+        while ((x&(x-1))!=0){
+            c++;
+            x=x&(x-1);
+        }
+
+        return c+1;
+    }
+};
