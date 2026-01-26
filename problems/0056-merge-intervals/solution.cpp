0b4093b8d2c1bda226adaa499fe92918a5478672
akshat-bajpai <akshat24056@iiitd.ac.in>
2026-01-26 19:09:07 +0000

    [LeetCode Sync] Runtime - 3 ms (88.74%), Memory - 24 MB (31.19%)

--- problems/0056-merge-intervals/solution.cpp
+++ problems/0056-merge-intervals/solution.cpp
@@ -4,13 +4,23 @@ public:
         sort(intervals.begin(),intervals.end());
         int n=intervals.size();
         vector<vector<int>> ans;
+
+        vector<int> temp;
         for (int i=0;i<n;i++){
-            if (ans.empty() || ans.back()[1]<intervals[i][0]){
-                ans.push_back(intervals[i]);
+            if (temp.size()==0){
+                temp.push_back(intervals[i][0]);
+                temp.push_back(intervals[i][1]);
             }else{
-                ans.back()[1]=max(ans.back()[1],intervals[i][1]);
+                if (temp[1]>=intervals[i][0]){
+                    temp[1]=max(intervals[i][1],temp[1]);
+                }else{
+                    ans.push_back(temp);
+                    temp[0]=intervals[i][0];
+                    temp[1]=intervals[i][1];
+                }
             }
         }
+        ans.push_back(temp);
         return ans;
     }
 };
