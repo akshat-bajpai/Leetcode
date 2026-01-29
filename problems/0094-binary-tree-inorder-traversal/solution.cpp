12af54d88b740e3c974072afcc2c17871a7e1771
akshat-bajpai <akshat24056@iiitd.ac.in>
2026-01-29 14:08:26 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 11 MB (34.81%)

--- problems/0094-binary-tree-inorder-traversal/solution.cpp
+++ problems/0094-binary-tree-inorder-traversal/solution.cpp
@@ -13,19 +13,23 @@ class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
         vector<int> ans;
-
         if (root==NULL) return ans;
-
-        vector<int> left=inorderTraversal(root->left);
-        vector<int>right=inorderTraversal(root->right);
-        for (int i=0;i<left.size();i++){
-            ans.push_back(left[i]);
+        stack<TreeNode*> st;
+        TreeNode* node=root;
+        
+        while (true){
+            if (node!=NULL){
+                st.push(node);
+                node=node->left;
+            }else{
+                if (st.empty()) break;
+                node=st.top();
+                ans.push_back(node->val);
+                st.pop();
+                node=node->right;
+                
+            }
         }
-        ans.push_back(root->val);
-        for (int i=0;i<right.size();i++){
-            ans.push_back(right[i]);
-        }
-
         return ans;
     }
 };
