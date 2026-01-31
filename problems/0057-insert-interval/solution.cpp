eb3468c3d67d117d6c093644530deff18b06fb3e
akshat-bajpai <akshat24056@iiitd.ac.in>
2026-01-31 21:40:29 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 21.8 MB (48.91%)

--- problems/0057-insert-interval/solution.cpp
+++ problems/0057-insert-interval/solution.cpp
@@ -2,27 +2,35 @@ class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
         int n=intervals.size();
-        int i=0;
         vector<vector<int>> ans;
+        if (intervals.size()==0){
+            ans.push_back(newInterval);
+            return ans;
+        } 
+        int i=0;
 
         while (i<n && intervals[i][1]<newInterval[0]){
-            ans.push_back({intervals[i][0],intervals[i][1]});
+            ans.push_back(intervals[i]);
             i++;
         }
-        while (i<n && intervals[i][0]<=newInterval[1]){
-            newInterval[0]=min(newInterval[0],intervals[i][0]);
-            newInterval[1]=max(newInterval[1],intervals[i][1]);
+        if (i == n) {
+            ans.push_back(newInterval);
+            return ans;
+        }
+
+        vector<int> temp=newInterval;
+
+        while(i<n && temp[1]>=intervals[i][0]){
+            temp[0]=min(temp[0],intervals[i][0]);
+            temp[1]=max(temp[1],intervals[i][1]);
             i++;
         }
-        ans.push_back(newInterval);
+        ans.push_back(temp);
 
         while (i<n){
-            ans.push_back({intervals[i][0],intervals[i][1]});
+            ans.push_back(intervals[i]);    
             i++;
         }
-
         return ans;
-
-
     }
 };
