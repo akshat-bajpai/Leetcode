c05490f114953b1273a370f9c4804d4b41b6ed59
akshat-bajpai <akshat24056@iiitd.ac.in>
2026-02-02 12:09:53 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 20.5 MB (96.84%)

--- problems/0045-jump-game-ii/solution.cpp
+++ problems/0045-jump-game-ii/solution.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        int maxi=0;
         int jumps=0;
+        int maxi=0;
         int curr=0;
 
         for (int i=0;i<nums.size()-1;i++){
-            maxi=max(maxi,i+nums[i]);
+            maxi=max(i+nums[i],maxi);
 
             if (i==curr){
-                jumps++;
                 curr=maxi;
+                jumps++;
             }
         }
-
         return jumps;
     }
 };
