b5542f47b19ada76dd180f11dda405acd7cc0779
akshat-bajpai <akshat24056@iiitd.ac.in>
2026-05-18 17:39:01 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 10.3 MB (74.79%)

--- problems/0213-house-robber-ii/solution.cpp
+++ problems/0213-house-robber-ii/solution.cpp
@@ -1,28 +1,35 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        int prev=nums[0];
+        //case 1: we dont take n-1th and take 0th
         int prev2=0;
+        int prev=nums[0];
+        int curi=nums[0];
         for (int i=1;i<nums.size()-1;i++){
-            int take=nums[i]+prev2;
+            int take=prev2+nums[i];
             int notTake=prev;
-            int curi=max(take,notTake);
+            curi=max(take,notTake);
             prev2=prev;
-            prev=curi;;
+            prev=curi;
         }
-        int case1= prev;
-        if (nums.size()>=2)prev=nums[1];
-        else return nums[0];
-        prev2=0;
-        for (int i=2;i<nums.size();i++){
-            int take=nums[i]+prev2;
-            int notTake=prev;
-            int curi=max(take,notTake);
-            prev2=prev;
-            prev=curi;;
-        }
-        int case2= prev;
 
-        return max(case1,case2);
+        int case1=curi;
+        //case 1 dont take 0th
+
+        prev2=0;
+        if (nums.size()==1) return nums[0];
+        prev=nums[1];
+        curi=prev;
+        for (int i=2;i<nums.size();i++){
+            int take=prev2+nums[i];
+            int notTake=prev;
+            curi=max(take,notTake);
+            prev2=prev;
+            prev=curi;
+        }
+
+        int case2=curi;
+
+        return max(case1, case2);
     }
 };
