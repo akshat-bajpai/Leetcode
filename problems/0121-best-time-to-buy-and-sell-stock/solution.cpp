0ee4128fb5b4537f282ab37c28890608b8b311df
akshat-bajpai <akshat24056@iiitd.ac.in>
2026-05-23 05:34:50 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 97.4 MB (61.38%)

--- problems/0121-best-time-to-buy-and-sell-stock/solution.cpp
+++ problems/0121-best-time-to-buy-and-sell-stock/solution.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int min=prices[0];
+        int n=prices.size();
+        int mini=prices[0];
         int max_profit=0;
-        for (int i=0;i<prices.size();i++){
-            if (prices[i]<min){
-                min=prices[i];
+
+        for (int i=1;i<n;i++){
+            if (prices[i]<mini){
+                mini=prices[i];
             }
-            if (prices[i]-min>max_profit){
-                max_profit=prices[i]-min;
+            if (prices[i]-mini>max_profit){
+                max_profit=prices[i]-mini;
             }
         }
-        return max_profit;
 
+        return max_profit;
     }
 };
