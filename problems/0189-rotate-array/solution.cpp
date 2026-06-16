10a9c98b973b836d50ea75fac27bd33c09f7ba4e
akshat-bajpai <akshat24056@iiitd.ac.in>
2026-06-16 08:55:46 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 29.6 MB (46.82%)

--- problems/0189-rotate-array/solution.cpp
+++ problems/0189-rotate-array/solution.cpp
@@ -1,19 +1,9 @@
 class Solution {
 public:
-    void reverse(vector<int>& arr,int start,int end){
-        while (start<end){
-            int temp=arr[start];
-            arr[start]=arr[end];
-            arr[end]=temp;
-            start++;
-            end--;
-        }
-    }
     void rotate(vector<int>& nums, int k) {
-        int n=nums.size();
-        reverse(nums,0,n-k%n-1);
-        reverse(nums,n-k%n,n-1);
-        reverse(nums,0,n-1);
+        k=k%nums.size();
+        reverse(nums.begin(),nums.begin()+nums.size()-k);
+        reverse(nums.begin()+nums.size()-k,nums.end());
+        reverse(nums.begin(),nums.end());
     }
-        
 };
