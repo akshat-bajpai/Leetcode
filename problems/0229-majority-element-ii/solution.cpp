122b6f027a222d1c7dc34baff1f7377d43b82a9d
akshat-bajpai <akshat24056@iiitd.ac.in>
2026-06-18 12:12:03 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 23.6 MB (72.40%)

--- problems/0229-majority-element-ii/solution.cpp
+++ problems/0229-majority-element-ii/solution.cpp
@@ -2,27 +2,38 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         int el1=INT_MIN;
-        int cnt1=0;
         int el2=INT_MIN;
-        int cnt2=0;
-        vector<int> ans;
-
-        for (int i=0;i<nums.size();i++){
-            if (cnt1==0 && nums[i]!=el2){
-                cnt1++;
-                el1=nums[i];
-            }else  if(cnt2==0 && nums[i]!=el1){
-                cnt2++;
-                el2=nums[i];
-            }else if(nums[i]==el1) cnt1++;
-            else if (nums[i]==el2) cnt2++;
-            else{
-                cnt1--;
-                cnt2--;
-            }
-        }
         int c1=0;
         int c2=0;
+        for (int i=0;i<nums.size();i++){
+            if (nums[i]!=el2 && el1==INT_MIN){
+                el1=nums[i];
+                c1++;
+                continue;
+            }
+            if (nums[i]!=el1 && el2==INT_MIN){
+                el2=nums[i];
+                c2++;
+                continue;
+            }
+
+            if (nums[i]==el1){
+                c1++;
+            }else if(nums[i]==el2){
+                c2++;
+            }else{
+                c1--;c2--;
+            }
+            if (c1==0){
+                el1=INT_MIN;
+            }
+            if (c2==0){
+                el2=INT_MIN;
+            }
+        }
+        vector<int> ans;
+        c1=0;
+        c2=0;
         for (int i=0;i<nums.size();i++){
             if (nums[i]==el1) c1++;
             if (nums[i]==el2) c2++;
@@ -30,5 +41,6 @@ public:
         if (c1>nums.size()/3) ans.push_back(el1);
         if (c2>nums.size()/3) ans.push_back(el2);
         return ans;
+
     }
 };
