bfb440c61c06c7085692b9d4ce561d7485ed431d
akshat-bajpai <akshat24056@iiitd.ac.in>
2026-06-27 14:28:55 +0000

    [LeetCode Sync] Runtime - 7 ms (51.70%), Memory - 15 MB (86.58%)

--- problems/0138-copy-list-with-random-pointer/solution.cpp
+++ problems/0138-copy-list-with-random-pointer/solution.cpp
@@ -1,47 +1,31 @@
-/*
-// Definition for a Node.
-class Node {
-public:
-    int val;
-    Node* next;
-    Node* random;
-    
-    Node(int _val) {
-        val = _val;
-  
-        next = NULL;
-        random = NULL;
-    }
-};
-*/
-
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
+
         Node* temp=head;
-        while (temp!=nullptr){
+        while (temp){
             Node* newNode=new Node(temp->val);
             newNode->next=temp->next;
             temp->next=newNode;
             temp=temp->next->next;
         }
-
         temp=head;
-        while (temp!=nullptr){
+        Node* dummy=new Node(-1);
+        Node* res=dummy;
+        while (temp){
             if (temp->random) temp->next->random=temp->random->next;
-            else temp->next->random=nullptr;
-            temp=temp->next->next;
+            temp=temp->next->next;        
         }
         temp=head;
-        Node* res;
-        Node* dummy=new Node(-1);
-        res=dummy;
-        while (temp!=nullptr){
+        while (temp){
             res->next=temp->next;
-            res=res->next;
             temp->next=temp->next->next;
             temp=temp->next;
+            res=res->next;
         }
+        
+
         return dummy->next;
+
     }
 };
