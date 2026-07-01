93b1e13c0de960e21589ed6e9c469275d366a56f
akshat-bajpai <akshat24056@iiitd.ac.in>
2026-07-01 11:12:53 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 23 MB (84.30%)

--- problems/0110-balanced-binary-tree/solution.cpp
+++ problems/0110-balanced-binary-tree/solution.cpp
@@ -12,15 +12,13 @@
 class Solution {
 public:
     bool isBalanced(TreeNode* root) {
-        return (dHeight(root)!=-1);
+        return dHeight(root)!=-1;
     }
-
     int dHeight(TreeNode* root){
         if (root==NULL) return 0;
         int lh=dHeight(root->left);
         int rh=dHeight(root->right);
-        if (lh==-1 || rh==-1) return -1;
-        if (abs(lh-rh)>1) return -1;
-        return (max(lh,rh))+1;
+        if (lh==-1 || rh==-1 || abs(lh-rh)>1) return -1;
+        return max(lh,rh)+1;
     }
 };
