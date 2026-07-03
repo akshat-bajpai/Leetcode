b8fab7b343e1c7980e052107e366f576c06fcd3a
akshat-bajpai <akshat24056@iiitd.ac.in>
2026-07-03 06:50:52 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 13.7 MB (44.07%)

--- problems/0035-search-insert-position/solution.cpp
+++ problems/0035-search-insert-position/solution.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        int ans=nums.size();
-        int low=0;
+        int low=0;  
         int high=nums.size()-1;
-        while(low<=high){
+        int a=nums.size()-1;
+        while (low<=high){
             int mid=low+(high-low)/2;
-            if (nums[mid]>=target){
-                ans=mid;
-                high=mid-1;
-            }else{
+            if (nums[mid]==target) return mid;
+            else if (nums[mid]<target){
                 low=mid+1;
+            }else{
+                high=mid-1;
+                a=high;
             }
         }
-        return ans;
+        return a+1;
     }
 };
