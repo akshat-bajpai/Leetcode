c135c4220222f8977595118ac00d91d7e8579826
akshat-bajpai <akshat24056@iiitd.ac.in>
2026-07-03 08:16:57 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 22.9 MB (54.35%)

--- problems/0907-koko-eating-bananas/solution.cpp
+++ problems/0907-koko-eating-bananas/solution.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
     int minEatingSpeed(vector<int>& piles, int h) {
-        int maxi=INT_MIN;
-        for (int i=0;i<piles.size();i++){
-            maxi=max(maxi,piles[i]);
-        }
         int low=1;
-        int high=maxi;
-        int ans=0;
+        int high=piles[0];
+        for (int i=0;i<piles.size();i++){
+            high=max(high,piles[i]);
+        }
+        int ans=-1;
         while (low<=high){
             int mid=low+(high-low)/2;
-            long long hours=0;
+            long long time=0;
             for (int i=0;i<piles.size();i++){
-                hours+=ceil((double)piles[i]/mid);
+                time+=(piles[i]+mid-1)/mid;
             }
-            if (hours>h) low=mid+1;
-            else if (hours<=h){
+            if (time<=h) {
                 ans=mid;
                 high=mid-1;
             }
-            
+            else if (time>h) low=mid+1;
+            else high=mid-1;
         }
         return ans;
     }
