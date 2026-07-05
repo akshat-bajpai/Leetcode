fc7c77ff8faafb2d967f4d22816fc73cc8990ab0
akshat-bajpai <akshat24056@iiitd.ac.in>
2026-07-05 12:55:27 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 15.1 MB (69.21%)

--- problems/0033-search-in-rotated-sorted-array/solution.cpp
+++ problems/0033-search-in-rotated-sorted-array/solution.cpp
@@ -6,21 +6,27 @@ public:
         while (low<=high){
             int mid=low+(high-low)/2;
             if (nums[mid]==target) return mid;
-            else{
-                if (nums[low]<=nums[mid]){
-                    if (nums[low]<=target && target<=nums[mid]){
-                        high=mid-1;
-                    }else{
-                        low=mid+1;
-                    }
+            else if (nums[mid]>target){
+                if (nums[low]>nums[mid]){
+                    high=mid-1;
                 }else{
-                    if (nums[mid]<=target && target<=nums[high]){
-                        low=mid+1;
-                    }else{
+                    if (target>=nums[low]){
                         high=mid-1;
+                    }else{
+                        low=mid+1;
                     }
                 }
-            }
+            }else{ //nums[mid]<target
+                if (nums[high]>nums[mid]){
+                    if (nums[high]>=target){
+                        low=mid+1;
+                    }else{
+                        high=mid-1;
+                    }
+                }else{
+                    low=mid+1;
+                }
+            }   
         }
         return -1;
     }
