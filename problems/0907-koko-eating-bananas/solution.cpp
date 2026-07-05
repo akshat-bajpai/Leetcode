549b1b64a389af7e3f8b7fbe3555dcdb584bfe04
akshat-bajpai <akshat24056@iiitd.ac.in>
2026-07-05 18:39:53 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 23 MB (20.00%)

--- problems/0907-koko-eating-bananas/solution.cpp
+++ problems/0907-koko-eating-bananas/solution.cpp
@@ -9,16 +9,15 @@ public:
         int ans=-1;
         while (low<=high){
             int mid=low+(high-low)/2;
-            long long time=0;
+            long long tot=0;
             for (int i=0;i<piles.size();i++){
-                time+=(piles[i]+mid-1)/mid;
+                tot+=(piles[i]+mid-1)/mid;
             }
-            if (time<=h) {
+            if (tot>h) low=mid+1;
+            else{
                 ans=mid;
                 high=mid-1;
             }
-            else if (time>h) low=mid+1;
-            else high=mid-1;
         }
         return ans;
     }
