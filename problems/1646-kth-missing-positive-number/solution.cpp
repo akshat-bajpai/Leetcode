cdd4db6070ad328f15ad8f6a95d655fd83d0b7df
akshat-bajpai <akshat24056@iiitd.ac.in>
2026-07-06 09:06:02 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 13.2 MB (93.24%)

--- problems/1646-kth-missing-positive-number/solution.cpp
+++ problems/1646-kth-missing-positive-number/solution.cpp
@@ -5,9 +5,11 @@ public:
         int high=arr.size()-1;
         while (low<=high){
             int mid=low+(high-low)/2;
-            int missing=arr[mid]-(mid+1);
-            if (missing<k) low=mid+1;
-            else high=mid-1;
+            if (arr[mid]-mid-1<k){
+                low=mid+1;
+            }else{
+                high=mid-1;
+            }
         }
         return low+k;
     }
