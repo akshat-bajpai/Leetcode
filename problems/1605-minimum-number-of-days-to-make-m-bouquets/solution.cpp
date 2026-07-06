38f6b5aaebd12fec5272c4e6d029cb3f37fecfd4
akshat-bajpai <akshat24056@iiitd.ac.in>
2026-07-06 04:48:06 +0000

    [LeetCode Sync] Runtime - 19 ms (80.16%), Memory - 70.4 MB (80.63%)

--- problems/1605-minimum-number-of-days-to-make-m-bouquets/solution.cpp
+++ problems/1605-minimum-number-of-days-to-make-m-bouquets/solution.cpp
@@ -1,41 +1,40 @@
 class Solution {
 public:
-    bool blooms(vector<int>&bloomDay,int m,int k, int day){
-        int boq=m;
-        int c=0;
+    bool possible(vector<int>& bloomDay, int m, int k, int day){
+        int tot=0;
+        int curr=0;
         for (int i=0;i<bloomDay.size();i++){
-            if (i==0 && bloomDay[i]<=day)c++;
-            else{
-                if (bloomDay[i]<=day)c++;
-                else c=0;
+            if (bloomDay[i]<=day){
+                curr++;
+                if (curr==k){
+                    tot++;
+                    curr=0;
+                }
+            }else{
+                curr=0;
             }
-            if (c==k){
-                boq--;
-                c=0;
-            }
-            if (boq<=0) return true;
         }
+        if (tot>=m) return true;
         return false;
     }
     int minDays(vector<int>& bloomDay, int m, int k) {
-       
-        int high=INT_MIN;
-        int low=INT_MAX;
-        for (int i=0;i<bloomDay.size();i++){
+        int n=bloomDay.size();
+        if ((long long)m*k>n) return -1;
+        int low=1;
+        int high=bloomDay[0];
+        for (int i=1;i<n;i++){
             high=max(high,bloomDay[i]);
-            low=min(low,bloomDay[i]);
         }
-        int ans=-1;
+
         while (low<=high){
             int mid=low+(high-low)/2;
-            if (blooms(bloomDay,m,k,mid)==true){
-                ans=mid;
+            if (possible(bloomDay,m,k,mid)){
                 high=mid-1;
             }else{
                 low=mid+1;
             }
         }
-        return ans; 
 
+        return low;
     }
 };
