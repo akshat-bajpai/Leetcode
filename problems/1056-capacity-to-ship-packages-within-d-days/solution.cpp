e82baccb97161f912a4cd90d94a635c6d3878d86
akshat-bajpai <akshat24056@iiitd.ac.in>
2026-07-06 06:28:30 +0000

    [LeetCode Sync] Runtime - 3 ms (99.09%), Memory - 35 MB (89.16%)

--- problems/1056-capacity-to-ship-packages-within-d-days/solution.cpp
+++ problems/1056-capacity-to-ship-packages-within-d-days/solution.cpp
@@ -1,40 +1,30 @@
 class Solution {
 public:
-    int shippable(vector<int>& weights,int days,int capacity){
-        int c=0;
-        int d=1;
-        for (int i=0;i<weights.size();i++){
-            c+=weights[i];
-            if (c<=capacity){
-
-            }else{
-                c=0;
-                c+=weights[i];
-                d++;
-            }
-            if (d>days) return false;
-        }
-        return true;
-    }
-
-
     int shipWithinDays(vector<int>& weights, int days) {
+        int low=weights[0];
         int high=0;
-        int low=INT_MIN;
         for (int i=0;i<weights.size();i++){
-            high+=weights[i];
             low=max(low,weights[i]);
+            high+=weights[i];
         }
-        int ans=low;
         while (low<=high){
             int mid=low+(high-low)/2;
-            if (shippable(weights,days,mid)==true){
-                ans=mid;
+            int d=1;
+            int cap=0;
+            for (int i=0;i<weights.size();i++){
+                if (cap+weights[i]<=mid){
+                    cap+=weights[i];
+                }else{
+                    cap=weights[i];
+                    d++;
+                }
+            }
+            if (d<=days){
                 high=mid-1;
             }else{
                 low=mid+1;
             }
         }
-        return ans;
+        return low;
     }
 };
