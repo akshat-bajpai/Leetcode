828d6ff861067a165a1ec788f2d9ae209c9c6a7b
akshat-bajpai <akshat24056@iiitd.ac.in>
2026-07-10 16:40:26 +0000

    [LeetCode Sync] Runtime - 34 ms (16.34%), Memory - 24.5 MB (13.13%)

--- problems/0200-number-of-islands/solution.cpp
+++ problems/0200-number-of-islands/solution.cpp
@@ -1,36 +1,39 @@
 class Solution {
 public:
-    void bfs(vector<vector<char>>& grid, vector<vector<int>>& vis, int i, int j  ){
-        vector<int> dr={1,-1,0,0};
-        vector<int> dy={0,0,1,-1};
+    void bfs(vector<vector<char>>& grid, vector<vector<int>>& vis, int i, int j){
+        int m=grid.size();
+        int n=grid[0].size();
         vis[i][j]=1;
+        int dr[]={1,-1,0,0};
+        int dc[]={0,0,1,-1};
         queue<pair<int,int>>q;
         q.push({i,j});
         while (!q.empty()){
-            pair<int,int> p=q.front();
+            auto node=q.front();
             q.pop();
             for (int i=0;i<4;i++){
-                int nrow=p.first+dr[i];
-                int ncol=p.second+dy[i];
-                if (nrow<0 || ncol<0 || nrow>=vis.size() || ncol>=vis[0].size()|| vis[nrow][ncol]==1 || grid[nrow][ncol]=='0') continue;
-                vis[nrow][ncol]=1;
-                q.push({nrow,ncol});
+                int nrow=node.first+dr[i];
+                int ncol=node.second+dc[i];
+                if (nrow>=0 && ncol>=0 && nrow<m && ncol<n && vis[nrow][ncol]==0 && grid[nrow][ncol]=='1'){
+                    q.push({nrow,ncol});
+                    vis[nrow][ncol]=1;
+                }
             }
         }
     }
     int numIslands(vector<vector<char>>& grid) {
         int m=grid.size();
         int n=grid[0].size();
-        int ans=0;
         vector<vector<int>> vis(m,vector<int>(n,0));
+        int num=0;
         for (int i=0;i<m;i++){
             for (int j=0;j<n;j++){
-                if (vis[i][j]==0 && grid[i][j]=='1'){
+                if (grid[i][j]=='1' && vis[i][j]==0){
+                    num++;
                     bfs(grid,vis,i,j);
-                    ans++;
                 }
             }
         }
-        return ans;
+        return num;
     }
 };
