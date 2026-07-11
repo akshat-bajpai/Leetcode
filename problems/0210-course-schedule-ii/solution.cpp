e594f82e203137307688c5af0bf63141d65f2a8d
akshat-bajpai <akshat24056@iiitd.ac.in>
2026-07-11 18:57:39 +0000

    [LeetCode Sync] Runtime - 7 ms (32.46%), Memory - 19 MB (19.93%)

--- problems/0210-course-schedule-ii/solution.cpp
+++ problems/0210-course-schedule-ii/solution.cpp
@@ -1,29 +1,33 @@
 class Solution {
 public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<int> ans;
-        vector<vector<int>>adj(numCourses);
-        for (int i=0;i<prerequisites.size();i++){
-            adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
+        vector<vector<int>> adj(numCourses);
+        for (auto it : prerequisites){
+            adj[it[1]].push_back(it[0]);
         }
-        vector<int>inDegree(numCourses,0);
-        for (int i=0;i<prerequisites.size();i++){
-            inDegree[prerequisites[i][0]]++;
+        vector<int> indegree(numCourses);
+        for (int i=0;i<numCourses;i++){
+            for (auto it : adj[i]){
+                indegree[it]++;
+            }
         }
         queue<int>q;
         for (int i=0;i<numCourses;i++){
-            if (inDegree[i]==0)q.push(i);
+            if (indegree[i]==0) q.push(i);
         }
+        vector<int> topo;
         while (!q.empty()){
             int node=q.front();
             q.pop();
+            topo.push_back(node);
             for (auto it : adj[node]){
-                inDegree[it]--;
-                if (inDegree[it]==0) q.push(it);
+                indegree[it]--;
+                if (indegree[it]==0){
+                    q.push(it);
+                }
             }
-            ans.push_back(node);
         }
-        if (ans.size()==numCourses) return ans;
-        else return {};
+        if (topo.size()==numCourses) return topo;
+        return {};
     }
 };
