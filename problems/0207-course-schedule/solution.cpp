5ffd61aea9a4ff8981ef42e0ee5b45224bbb776a
akshat-bajpai <akshat24056@iiitd.ac.in>
2026-07-11 12:57:29 +0000

    [LeetCode Sync] Runtime - 7 ms (45.16%), Memory - 20 MB (32.32%)

--- problems/0207-course-schedule/solution.cpp
+++ problems/0207-course-schedule/solution.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
-    bool dfsCycle(int numCourses,vector<vector<int>>& adj, int node, vector<int>& vis){
-        vis[node]=1;
-        for (auto it : adj[node]){
-            if (!vis[it]){
-                if (dfsCycle(numCourses,adj,it,vis)) return true;
-            }else{
-                if (vis[it]==1) return true;
-            }
-        }
-        vis[node]=2;
-        return false;
-    }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         vector<vector<int>> adj(numCourses);
-        for (int i=0;i<prerequisites.size();i++){
-            adj[prerequisites[i][0]].push_back(prerequisites[i][1]);
+        for (auto it : prerequisites){
+            adj[it[0]].push_back(it[1]);
         }
-        vector<int> vis(numCourses,0);
+        vector<int> indegree(numCourses);
+        for (int i=0;i<prerequisites.size();i++){
+            indegree[prerequisites[i][1]]++;
+        }
+        queue<int> q;
         for (int i=0;i<numCourses;i++){
-            if (!vis[i]){
-                if (dfsCycle(numCourses,adj,i,vis)) return false;
+            if (indegree[i]==0) q.push(i);
+        }
+        int topo=0;
+        while (!q.empty()){
+            int node=q.front();
+            topo++;
+            q.pop();
+            for (auto it : adj[node]){
+                indegree[it]--;
+                if (indegree[it]==0) q.push(it);
             }
         }
-        return true;
+        return topo==numCourses;
     }
 };
