10818bc655fdb6028238b4cf23a4970ba6208cb4
akshat-bajpai <akshat24056@iiitd.ac.in>
2026-07-12 15:45:27 +0000

    [LeetCode Sync] Runtime - 15 ms (71.62%), Memory - 25.4 MB (52.77%)

--- problems/1171-shortest-path-in-binary-matrix/solution.cpp
+++ problems/1171-shortest-path-in-binary-matrix/solution.cpp
@@ -1,33 +1,30 @@
 class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        // 1,1 1,0 1,-1 0,1 0,-1 -1,0 -1,-1 -1,1
-        if (grid[0][0]==1) return -1;
         int n=grid.size();
-        vector<int> drow={1,1,1,0,0,-1,-1,-1};
-        vector<int> dcol={1,0,-1,1,-1,0,-1,1};
-        //BFS
-        int length=1;
+        if (grid[0][0]==1 || grid[n-1][n-1]==1) return -1;
+        int dr[]={1,1,1,0,0,-1,-1,-1};
+        int dc[]={1,0,-1,-1,1,-1,0,1};
         queue<pair<int,int>> q;
-        q.push({0,0});
-        vector<vector<int>> vis (n,vector<int> (n,0));
+        vector<vector<int>> vis(n,vector<int>(n,0));
         vis[0][0]=1;
+        q.push({0,0});
+        int len=0;
         while (!q.empty()){
             int s=q.size();
-            for (int j=0;j<s;j++){
-                int r=q.front().first;
-                int c=q.front().second;
-                q.pop();
-                if (r==n-1 && c==n-1) return length;
+            len++;
+            for (int i=0;i<s;i++){
+                auto node=q.front();q.pop();
+                if (node.first==n-1 && node.second==n-1) return len;
                 for (int i=0;i<8;i++){
-                    int nrow=r+drow[i];
-                    int ncol=c+dcol[i];
-                    if (nrow<0 || ncol<0 || nrow>=n || ncol >=n || grid[nrow][ncol]==1 || vis[nrow][ncol]==1) continue;
-                    vis[nrow][ncol]=1;
-                    q.push({nrow,ncol});
+                    int nrow=node.first+dr[i];
+                    int ncol=node.second+dc[i];
+                    if (nrow>=0 && ncol>=0 && nrow<n && ncol<n && vis[nrow][ncol]==0 && grid[nrow][ncol]==0 ){
+                        vis[nrow][ncol]=1;
+                        q.push({nrow,ncol});
+                    }
                 }
             }
-            length++;
         }
         return -1;
     }
