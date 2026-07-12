ad1af7e55b550cd3d2cb291f6b3a06cd911a526d
akshat-bajpai <akshat24056@iiitd.ac.in>
2026-07-12 03:54:03 +0000

    [LeetCode Sync] Runtime - 36 ms (61.14%), Memory - 43.2 MB (73.45%)

--- problems/1256-rank-transform-of-an-array/solution.cpp
+++ problems/1256-rank-transform-of-an-array/solution.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
     vector<int> arrayRankTransform(vector<int>& arr) {
-        vector<int> temp;
-        temp=arr;
+        vector<int> temp(arr.begin(),arr.end());
         sort(temp.begin(),temp.end());
         unordered_map<int,int> mpp;
         int r=1;
-        for (int i=0;i<arr.size();i++){
+        for (int i=0;i<temp.size();i++){
             if (mpp.find(temp[i])==mpp.end()){
-                mpp[temp[i]]=r;
-                r++;
+                mpp[temp[i]]=r++;
             }
         }
         for (int i=0;i<arr.size();i++){
             temp[i]=mpp[arr[i]];
         }
-
         return temp;
     }
 };
