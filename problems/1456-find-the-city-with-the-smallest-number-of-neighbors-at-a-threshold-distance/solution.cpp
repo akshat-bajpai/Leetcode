fdc72f6990f4aaaf656cf906efa140f216b05e46
akshat-bajpai <akshat24056@iiitd.ac.in>
2026-07-15 07:46:18 +0000

    [LeetCode Sync] Runtime - 34 ms (8.74%), Memory - 21.2 MB (5.76%)

--- problems/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/solution.cpp
+++ problems/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/solution.cpp
@@ -2,40 +2,39 @@ class Solution {
 public:
     int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
         vector<vector<pair<int,int>>> adj(n);
-        for (int i=0;i<edges.size();i++){
-            adj[edges[i][0]].push_back({edges[i][1],edges[i][2]});
-            adj[edges[i][1]].push_back({edges[i][0],edges[i][2]});
+        for (auto it : edges){
+            adj[it[0]].push_back({it[1],it[2]});
+            adj[it[1]].push_back({it[0],it[2]});
         }
-        int smallestNumberOfCities=INT_MAX;
-        int ans=0;
-        for (int i=0;i<n;i++){
+        int answerCity=0;
+        int mini=INT_MAX;
+        for (int city=0;city<n;city++){
             vector<int> dist(n,1e9);
+            dist[city]=0;
             priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
-            dist[i]=0;
-            pq.push({0,i});
+            pq.push({0,city});
             while (!pq.empty()){
-                int node=pq.top().second;
                 int d=pq.top().first;
+                int node=pq.top().second;
                 pq.pop();
-                if (d>dist[node]) continue;
                 for (auto it : adj[node]){
                     int adjNode=it.first;
-                    int edgeWeight=it.second;
-                    if (dist[adjNode]>d+edgeWeight){
-                        dist[adjNode]=d+edgeWeight;
-                        pq.push({dist[adjNode],adjNode});
+                    int eW=it.second;
+                    if (dist[adjNode]>d+eW){
+                        dist[adjNode]=d+eW;
+                        pq.push({d+eW,adjNode});
                     }
                 }
             }
-            int count=0;
-            for (int j=0;j<n;j++){
-                if (dist[j]<=distanceThreshold && j!=i) count++;
+            int c=0;
+            for (int i=0;i<n;i++){
+                if (dist[i]<=distanceThreshold) c++;
             }
-            if (count<=smallestNumberOfCities){
-                ans=i;
-                smallestNumberOfCities=count;
+            if (c<=mini){
+                mini=c;
+                answerCity=city;
             }
         }
-        return ans;
+        return answerCity;
     }
 };
