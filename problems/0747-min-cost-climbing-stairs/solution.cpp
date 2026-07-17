ef6ec03db73da5086bad5fb76e8d45ee13aa7399
akshat-bajpai <akshat24056@iiitd.ac.in>
2026-07-17 07:27:35 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.7 MB (30.23%)

--- problems/0747-min-cost-climbing-stairs/solution.cpp
+++ problems/0747-min-cost-climbing-stairs/solution.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int minCostClimbingStairs(vector<int>& cost) {
+        //dp[i]=min cost to reach i;
         int n=cost.size();
-        vector<int> dp(cost.size()+1);
-        //dp[i]=min cost to reach i
-        dp[0]=cost[0];
-        dp[1]=cost[1];
+        vector<int> dp(n+1);
+        dp[0]=0;
+        dp[1]=0;
         for (int i=2;i<=n;i++){
-            dp[i]=(i==n?0:cost[i])+min(dp[i-1],dp[i-2]);
+            dp[i]=min(dp[i-1]+cost[i-1],dp[i-2]+cost[i-2]);
         }
         return dp[n];
     }
