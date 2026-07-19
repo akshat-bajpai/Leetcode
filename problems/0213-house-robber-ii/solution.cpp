ace9513d328ddd911a76d5c24498aa8592980b19
akshat-bajpai <akshat24056@iiitd.ac.in>
2026-07-19 18:10:28 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 11 MB (19.34%)

--- problems/0213-house-robber-ii/solution.cpp
+++ problems/0213-house-robber-ii/solution.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
-    int rob(vector<int>& nums) {
+    int robHelper(vector<int>& nums) {
         int n=nums.size();
-        //case 1: take 0 house not take n-1 house
-        //case 2 not take 0 house
-
-        vector<int> dp(n,0);
-        dp[0]=0; //not take;
-        for (int i=1;i<n;i++){
-            int take=nums[i]+(i==1?0:dp[i-2]);
+        if (n==0) return 0;
+        vector<int> dp(n);
+        //dp[i]=max profit robbing houses till index i
+        dp[0]=nums[0];
+        if (n==1) return dp[0];
+        dp[1]=max(nums[1],nums[0]);
+        for (int i=2;i<n;i++){
+            int take=dp[i-2]+nums[i];
             int notTake=dp[i-1];
             dp[i]=max(take,notTake);
         }
-
-        vector<int> dp2(n,0);
-        dp2[0]=nums[0];
-        for (int i=1;i<n-1;i++){
-            int take=nums[i] + (i==1?0:dp2[i-2]);
-            int notTake=dp2[i-1];
-            dp2[i]=max(take,notTake);
-        }
-        if (n==1) return nums[0];
-        return max(dp[n-1],dp2[n-2]);
+        return dp[n-1];
+    }
+    int rob(vector<int>& nums) {
+        if (nums.size()==1) return nums[0];
+        vector<int> left(nums.begin(),nums.end()-1);
+        vector<int> right(nums.begin()+1,nums.end());
+        return max(robHelper(left),robHelper(right));
     }
 };
