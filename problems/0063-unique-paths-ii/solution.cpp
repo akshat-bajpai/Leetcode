46ed3d88ab4ec2c1b617fe9cc0f08c6901a9bcf9
akshat-bajpai <akshat24056@iiitd.ac.in>
2026-07-20 06:53:06 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 11.9 MB (32.31%)

--- problems/0063-unique-paths-ii/solution.cpp
+++ problems/0063-unique-paths-ii/solution.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        int m=obstacleGrid.size(),n=obstacleGrid[0].size();
-        vector<int> prev(n,0);
+        int m=obstacleGrid.size();
+        int n=obstacleGrid[0].size();
+        if (obstacleGrid[0][0]==1) return 0;
+        vector<vector<int>> dp(m,vector<int>(n,0));
+        dp[0][0]=1;
         for (int i=0;i<m;i++){
-            vector<int> temp(n,0);
             for (int j=0;j<n;j++){
-                if (i==0 && j==0){
-                    if (obstacleGrid[i][j]==1) return 0;
-                    temp[j]=1;
-                }
-                if(obstacleGrid[i][j]==1) continue;
-                if (i-1>=0 && obstacleGrid[i-1][j]==0) temp[j]+=prev[j];
-                if (j-1>=0 && obstacleGrid[i][j-1]==0) temp[j]+=temp[j-1];
+                if (obstacleGrid[i][j]==1) continue;
+                if (i==0 && j==0) continue;
+                if (i>0 && obstacleGrid[i-1][j]==0) dp[i][j]+=dp[i-1][j];
+                if (j>0 && obstacleGrid[i][j-1]==0) dp[i][j]+=dp[i][j-1];
             }
-            prev=temp;
         }
-        return prev[n-1];
+        return dp[m-1][n-1];
+        
     }
 };
