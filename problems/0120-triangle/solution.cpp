3896bb317737ed0a8fdfb57eaf1a8b8fca6359f2
akshat-bajpai <akshat24056@iiitd.ac.in>
2026-07-24 13:27:27 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 13 MB (65.45%)

--- problems/0120-triangle/solution.cpp
+++ problems/0120-triangle/solution.cpp
@@ -2,20 +2,19 @@ class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n=triangle.size();
-        vector<vector<int>> dp(n,vector<int>(n));
-        dp[0][0]=triangle[0][0];
-        for (int i=1;i<n;i++){
-            for (int j=0;j<=i;j++){
-                int minSum=INT_MAX;
-                if (j!=0) minSum=min(minSum,triangle[i][j]+dp[i-1][j-1]);
-                if (j!=i) minSum=min(minSum,triangle[i][j]+dp[i-1][j]);
-                dp[i][j]=minSum;
-            }
-        }
-        int ans=dp[n-1][0];
+        vector<int> next(n,0);
         for (int i=0;i<n;i++){
-            ans=min(dp[n-1][i],ans);
+            next[i]=triangle[n-1][i];
         }
-        return ans;
+        for (int i=n-2;i>=0;i--){
+            vector<int> cur(n,0);
+            for (int j=0;j<=i;j++){
+                int d=triangle[i][j]+next[j];
+                int dg=triangle[i][j]+next[j+1];
+                cur[j]=min(d,dg);
+            }
+            next=cur;
+        }
+        return next[0];
     }
 };
