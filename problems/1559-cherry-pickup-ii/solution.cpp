2f2e67b7fc5558090e4e140a4bd54f96fa3d8c5e
akshat-bajpai <akshat24056@iiitd.ac.in>
2026-07-24 20:10:19 +0000

    [LeetCode Sync] Runtime - 115 ms (5.59%), Memory - 21.1 MB (29.89%)

--- problems/1559-cherry-pickup-ii/solution.cpp
+++ problems/1559-cherry-pickup-ii/solution.cpp
@@ -1,35 +1,29 @@
 class Solution {
 public:
     int cherryPickup(vector<vector<int>>& grid) {
-        int rows=grid.size();
-        int cols=grid[0].size();
-        //dp[i][j][r]=max cherries collected when robot 1 is at i, 2 is at j and their row is r
-        vector<vector<vector<int>>> dp(cols, vector<vector<int>>(cols,vector<int>(rows,-1)));
-        dp[0][cols-1][0]=(0==cols-1)?grid[0][0]:grid[0][0]+grid[0][cols-1];
-        //dp[i][j][r]
-        // -> dp[i][j][r-1]
-        // -> dp[i][j-1][r-1]
-        // -> dp[i][j+1][r-1]
-        for (int r=1;r<rows;r++){
-            for (int i=0;i<cols;i++){
-                for (int j=cols-1;j>=0;j--){
-                    for (int d1=-1;d1<=1;d1++){
-                        for (int d2=-1;d2<=1;d2++){
-                            int pi=i+d1;
-                            int pj=j+d2;
-                            if (pi<0 || pj<0 || pi>=cols || pj>= cols ||dp[pi][pj][r-1]==-1 ) continue;
-                            dp[i][j][r]=max(dp[i][j][r],dp[pi][pj][r-1]+(i==j?grid[r][i]:grid[r][i]+grid[r][j]));
+        int m=grid.size();
+        int n=grid[0].size();
+        vector<vector<vector<int>>> dp(m,vector<vector<int>>(n,vector<int>(n,0)));
+        for (int j1=0;j1<n;j1++){
+            for (int j2=0;j2<n;j2++){
+                if (j1==j2) dp[m-1][j1][j2]=grid[m-1][j1];
+                else dp[m-1][j1][j2]=grid[m-1][j1]+grid[m-1][j2];
+            }
+        }
+        for (int i=m-2;i>=0;i--){
+            for (int j1=0;j1<n;j1++){
+                for (int j2=0;j2<n;j2++){
+                    for (int dj1=-1;dj1<=1;dj1++){
+                        for (int dj2=-1;dj2<=1;dj2++){
+                            int nj1=j1+dj1;
+                            int nj2=j2+dj2;
+                            if (nj1<0 || nj2<0 || nj1>= n || nj2>=n) continue;
+                            dp[i][j1][j2]=max(dp[i][j1][j2], (j1==j2?grid[i][j1]+dp[i+1][j1+dj1][j2+dj2] : grid[i][j1]+grid[i][j2]+dp[i+1][j1+dj1][j2+dj2]));
                         }
                     }
                 }
             }
         }
-        int ans=dp[0][0][rows-1];
-        for (int i=0;i<cols;i++){
-            for (int j=0;j<cols;j++){
-                ans=max(ans,dp[i][j][rows-1]);
-            }
-        }
-        return ans;
+        return dp[0][0][n-1];
     }
 };
