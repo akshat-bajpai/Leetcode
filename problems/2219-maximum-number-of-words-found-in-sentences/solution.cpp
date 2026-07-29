b5c7aab118899e71ca64bb5cad35fc96c2c8a030
akshat-bajpai <akshat24056@iiitd.ac.in>
2026-07-29 08:56:49 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 12.9 MB (79.05%)

---
+++ problems/2219-maximum-number-of-words-found-in-sentences/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int mostWordsFound(vector<string>& sentences) {
+        int maxi=0;
+        for (int i=0;i<sentences.size();i++){
+            int words=0;
+            for (int j=0;j<sentences[i].size();j++){
+                if (sentences[i][j]==' '){
+                    words++;
+                }
+            }
+            words++;
+            maxi=max(maxi,words);
+
+        }
+        return maxi;
+    }
+};
