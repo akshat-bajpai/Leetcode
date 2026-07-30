77c802dcfc8fc599e56a73b9f09512f1c70173fb
akshat-bajpai <akshat24056@iiitd.ac.in>
2026-07-30 10:13:43 +0000

    [LeetCode Sync] Runtime - 23 ms (77.95%), Memory - 31.4 MB (71.62%)

--- problems/1437-minimum-insertion-steps-to-make-a-string-palindrome/solution.cpp
+++ problems/1437-minimum-insertion-steps-to-make-a-string-palindrome/solution.cpp
@@ -2,24 +2,28 @@ class Solution {
 public:
     int minInsertions(string s) {
         int n=s.size();
-        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
-        string r=s;
-        reverse(r.begin(),r.end());
-        for (int i=0;i<=n;i++){
-            dp[i][0]=0;
-            dp[0][i]=0;
+        //dp[i][j]=min number of steps to make s[i...j] a palindrome
+        //f(i,j)
+        //  if (i==j) returnn 0
+        //  if (s[i]==s[j]) return f(i+1,j-1)
+        //  int lnt=f(i+1,j)+1
+        //  int rnt=f(i,j-1)+1
+        // return min(lnt,rnt)
+        vector<vector<int>> dp(n,vector<int>(n,0));
+        for (int i=0;i<n;i++){
+            dp[i][i]=0;
         }
-        for (int i=1;i<=n;i++){
-            for (int j=1;j<=n;j++){
-                if (s[i-1]==r[j-1]){
-                    dp[i][j]=1+dp[i-1][j-1];
-                    continue;
+        for (int i=n-1;i>=0;i--){
+            for (int j=i+1;j<n;j++){
+                if (s[i]==s[j]){
+                    dp[i][j]=dp[i+1][j-1];
+                }else{
+                    int lnt=dp[i+1][j];
+                    int rnt=dp[i][j-1];
+                    dp[i][j]=min(lnt,rnt)+1;
                 }
-                dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
             }
         }
-        int subSize=dp[n][n];
-
-        return n-subSize;
+        return dp[0][n-1];
     }
 };
