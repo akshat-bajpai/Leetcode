fe2a06017ca2664df947ced88d7b234c80a5146b
akshat-bajpai <akshat24056@iiitd.ac.in>
2026-08-02 21:35:56 +0000

    [LeetCode Sync] Runtime - 9 ms (39.35%), Memory - 88.5 MB (22.12%)

---
+++ problems/2766-find-the-prefix-common-array-of-two-arrays/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
+        int n=A.size();
+        unordered_map<int,int> mpp;
+        vector<int> ans;
+        int count=0;
+        for (int i=0;i<n;i++){
+            mpp[A[i]]++;
+            if (mpp[A[i]]==2){
+                count++;
+            }
+            mpp[B[i]]++;
+            if (mpp[B[i]]==2){
+                count++;
+            }
+            ans.push_back(count);
+        }
+        return ans;
+    }
+};
