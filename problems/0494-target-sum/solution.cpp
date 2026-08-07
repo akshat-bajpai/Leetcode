1d19ad0d17c44adccbaba54363109656525b85ee
akshat-bajpai <akshat24056@iiitd.ac.in>
2026-08-07 13:23:24 +0000

    [LeetCode Sync] Runtime - 4 ms (76.73%), Memory - 15.6 MB (40.24%)

--- problems/0494-target-sum/solution.cpp
+++ problems/0494-target-sum/solution.cpp
@@ -1,36 +1,31 @@
 class Solution {
 public:
-    int helper(vector<int>& arr, int target){
-        int n=arr.size();
-        vector<vector<int>> dp(n,vector<int>(target+1,0));
-        //dp[i][j]=number of subsets forming sum j till index i
-        if (arr[0]==0) dp[0][0]=2;
-        else{
+    int helper(vector<int>& nums, int target){
+        int n=nums.size();
+        vector<vector<int>> dp(n,vector<int>(target+1,0)); //number of ways to make j using coins till index i
+        if (nums[0]==0){
+            dp[0][0]=2;
+        }else{
             dp[0][0]=1;
-            if (arr[0]<=target) dp[0][arr[0]]=1;
+            if (nums[0]<=target) dp[0][nums[0]]=1;
         }
         for (int i=1;i<n;i++){
             for (int j=0;j<=target;j++){
                 int notTake=dp[i-1][j];
                 int take=0;
-                if (j-arr[i]>=0) take=dp[i-1][j-arr[i]];
-                dp[i][j]=take+notTake;
+                if (j-nums[i]>=0) take=dp[i-1][j-nums[i]];
+                dp[i][j]=notTake+take;
             }
         }
         return dp[n-1][target];
     }
-
-
     int findTargetSumWays(vector<int>& nums, int target) {
-        // s1-s2=target
-        // s1+s2=sum
-        // s1=(target+sum)/2
         int sum=0;
-        for(int i=0;i<nums.size();i++){
+        for (int i=0;i<nums.size();i++){
             sum+=nums[i];
-        }        
-        if ((target+sum)%2==1 || target+sum<0) return 0;
-        target=(target+sum)/2;
-        return helper(nums,target);
+        }
+        if ((sum+target)%2==1) return 0;
+        if (sum+target<0) return 0;
+        return helper(nums,(sum+target)/2);
     }
 };
