87c193d0d99ea749f9d1c039b8ea6c153617690f
akshat-bajpai <akshat24056@iiitd.ac.in>
2026-08-09 17:52:19 +0000

    [LeetCode Sync] Runtime - 6 ms (88.82%), Memory - 16.7 MB (55.65%)

--- problems/0583-delete-operation-for-two-strings/solution.cpp
+++ problems/0583-delete-operation-for-two-strings/solution.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
     int minDistance(string word1, string word2) {
-        //f(i,j) min steps to make word1[0...i] and word2[0...j] the same
-        //  if (s[i]==s[j]) return f(i-1)
-        //  int df1=f(i-1,j)+1
-        //  int df2=f(i,j-1)+1
-        //  return df1+df2
         int n1=word1.size();
         int n2=word2.size();
-        vector<vector<int>> dp(n1+1,vector<int>(n2+1,0));
+        //f(i,j)==> min steps req to make word1[0...i] and word2[0...j] the same
+        //  if (i<0) return j+1
+        //  if (j<0) return i+1
+        // if (word1[i]==word2[j]) return f(i-1,j-1);
+        // else return min(f(i-1,j),f(i,j-1))
+        vector<vector<int>> dp(n1+1, vector<int>(n2+1,0));
         for (int i=0;i<=n1;i++){
             dp[i][0]=i;
         }
-        for (int i=0;i<=n2;i++){
-            dp[0][i]=i;
+        for (int j=0;j<=n2;j++){
+            dp[0][j]=j;
         }
         for (int i=1;i<=n1;i++){
             for (int j=1;j<=n2;j++){
                 if (word1[i-1]==word2[j-1]){
                     dp[i][j]=dp[i-1][j-1];
                 }else{
-                    dp[i][j]=min(dp[i-1][j],dp[i][j-1])+1;
+                    dp[i][j]=1+min(dp[i-1][j],dp[i][j-1]);
                 }
             }
         }
