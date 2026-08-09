02a312f1605ed0c54ba3c9176ce7aae66f2edc97
akshat-bajpai <akshat24056@iiitd.ac.in>
2026-08-09 17:17:03 +0000

    [LeetCode Sync] Runtime - 23 ms (77.89%), Memory - 31.3 MB (74.83%)

--- problems/1437-minimum-insertion-steps-to-make-a-string-palindrome/solution.cpp
+++ problems/1437-minimum-insertion-steps-to-make-a-string-palindrome/solution.cpp
@@ -1,29 +1,19 @@
 class Solution {
 public:
     int minInsertions(string s) {
-        int n=s.size();
-        //dp[i][j]=min number of steps to make s[i...j] a palindrome
-        //f(i,j)
-        //  if (i==j) returnn 0
+        //f(i,j) ==> min number of insertions to make s[i...j] a palindrome
+        //  if (i>=j) return 0
+        //
         //  if (s[i]==s[j]) return f(i+1,j-1)
-        //  int lnt=f(i+1,j)+1
-        //  int rnt=f(i,j-1)+1
-        // return min(lnt,rnt)
+        //  else return 1+min(f(i+1,j),f(i,j-1))
+        int n=s.size(); 
         vector<vector<int>> dp(n,vector<int>(n,0));
-        for (int i=0;i<n;i++){
-            dp[i][i]=0;
-        }
         for (int i=n-1;i>=0;i--){
             for (int j=i+1;j<n;j++){
-                if (s[i]==s[j]){
-                    dp[i][j]=dp[i+1][j-1];
-                }else{
-                    int lnt=dp[i+1][j];
-                    int rnt=dp[i][j-1];
-                    dp[i][j]=min(lnt,rnt)+1;
-                }
+                if (s[i]==s[j]) dp[i][j]=dp[i+1][j-1];
+                else dp[i][j]=1+min(dp[i+1][j],dp[i][j-1]);
             }
-        }
-        return dp[0][n-1];
+        }     
+        return dp[0][n-1];           
     }
 };
