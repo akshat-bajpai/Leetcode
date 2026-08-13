794d5153de35dd73a5063d9afc1d257d0f1c4a07
akshat-bajpai <akshat24056@iiitd.ac.in>
2026-08-13 15:17:29 +0000

    [LeetCode Sync] Runtime - 30 ms (58.67%), Memory - 44.2 MB (32.46%)

--- problems/0115-distinct-subsequences/solution.cpp
+++ problems/0115-distinct-subsequences/solution.cpp
@@ -1,24 +1,23 @@
+
 class Solution {
 public:
     int numDistinct(string s, string t) {
-        //dp[i][j]=number of distinct subsequences of s till i which equal to t till j
-        int n=s.size();
-        int m=t.size();
-        vector<vector<unsigned long long>> dp(n+1,vector<unsigned long long>(m+1));
-        for (int i=0;i<=m;i++){
-            dp[0][i]=0;
-        }
-        for (int i=0;i<=n;i++){
+        int n1=s.size();
+        int n2=t.size();
+        vector<vector<unsigned long long>> dp(n1+1,vector<unsigned long long>(n2+1,0));
+        for (int i=0;i<=n1;i++){
             dp[i][0]=1;
         }
-        for (int i=1;i<=n;i++){
-            for (int j=1;j<=m;j++){
+        for (int i=1;i<=n1;i++){
+            for (int j=1;j<=n2;j++){
                 if (s[i-1]==t[j-1]){
-                    dp[i][j]=dp[i-1][j-1];
+                    dp[i][j]+=dp[i-1][j-1];
+                    dp[i][j]+=dp[i-1][j];
+                }else{
+                    dp[i][j]+=dp[i-1][j];
                 }
-                dp[i][j]+=dp[i-1][j];
             }
         }
-        return dp[n][m];
+        return dp[n1][n2];
     }
 };
