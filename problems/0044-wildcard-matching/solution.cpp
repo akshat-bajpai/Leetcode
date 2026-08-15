4e0fd42e306e05636845045a39b9651671eb526f
akshat-bajpai <akshat24056@iiitd.ac.in>
2026-08-15 11:48:29 +0000

    [LeetCode Sync] Runtime - 57 ms (42.26%), Memory - 15.7 MB (67.52%)

--- problems/0044-wildcard-matching/solution.cpp
+++ problems/0044-wildcard-matching/solution.cpp
@@ -28,11 +28,10 @@ public:
         for (int i=1;i<=n1;i++){
             for (int j=1;j<=n2;j++){
                 if (s[i-1]==p[j-1]) dp[i][j]=dp[i-1][j-1];
-                else{
-                    if (p[j-1]=='?') dp[i][j]=dp[i-1][j-1];
-                    else if (p[j-1]=='*') dp[i][j]=dp[i-1][j]||dp[i][j-1];
-                    else dp[i][j]=false;
-                }
+                else if (p[j-1]=='?') dp[i][j]=dp[i-1][j-1];
+                else if (p[j-1]=='*') dp[i][j]=dp[i-1][j]||dp[i][j-1];
+                else dp[i][j]=false;
+                
             }
         }
         return dp[n1][n2];
