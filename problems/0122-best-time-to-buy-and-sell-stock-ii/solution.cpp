588fdccfd6214a5feee0ff19767543206e05b3c9
akshat-bajpai <akshat24056@iiitd.ac.in>
2026-08-15 14:42:36 +0000

    [LeetCode Sync] Runtime - 7 ms (39.68%), Memory - 24.6 MB (37.13%)

--- problems/0122-best-time-to-buy-and-sell-stock-ii/solution.cpp
+++ problems/0122-best-time-to-buy-and-sell-stock-ii/solution.cpp
@@ -1,21 +1,36 @@
+/*
+f(i,buy) => maximum profit achieved starting day i with (buy==1)? can buy and dont hold stock : cannot buy and holding stock
+    if (i==n-1)
+        if (buy==0) return prices[i]
+        else return 0
+    if (buy==0)
+        sell=prices[i]+f(i+1,1)
+        dontsell=f(i+1,0)
+        return max(sell,notsell)
+
+    else
+        buy=-prices[i]+f(i+1,0)
+        dontbuy=f(i+1,1)
+        return max(buy,notbuy)
+*/
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        //dp[i][buy]=maximum profit starting at ith index and buy constraint=buy (0=can not buy, 1=can buy)
         int n=prices.size();
-        vector<int> next(2),cur(2);
-        next[0]=0;
-        next[1]=0;
-        for (int i=n-1;i>=0;i--){
+        vector<vector<int>> dp(n,vector<int>(2));
+        for (int i=0;i<n;i++){
+            dp[i][0]=prices[i];
+            dp[i][1]=0;
+        }
+        for (int i=n-2;i>=0;i--){
             for (int j=0;j<2;j++){
-                if (j==1){
-                    cur[j]=max(next[1],next[0]-prices[i]);
+                if (j==0){
+                    dp[i][j]=max(prices[i]+dp[i+1][1],dp[i+1][0]);
                 }else{
-                    cur[j]=max(next[0],next[1]+prices[i]);
+                    dp[i][j]=max(-prices[i]+dp[i+1][0],dp[i+1][1]);
                 }
             }
-            next=cur;
         }
-        return next[1];
+        return dp[0][1];
     }
 };
