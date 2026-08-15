a78b67aca9073eedcbaf27b6ca5d7081a47c5207
akshat-bajpai <akshat24056@iiitd.ac.in>
2026-08-15 17:34:30 +0000

    [LeetCode Sync] Runtime - 11 ms (43.89%), Memory - 18.3 MB (28.65%)

--- problems/0188-best-time-to-buy-and-sell-stock-iv/solution.cpp
+++ problems/0188-best-time-to-buy-and-sell-stock-iv/solution.cpp
@@ -1,30 +1,26 @@
+/*
+
+*/
+
 class Solution {
 public:
-    int maxProfit(int limit, vector<int>& prices) {
+    int maxProfit(int k, vector<int>& prices) {
         int n=prices.size();
-        vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(2,vector<int>(limit+1)));
-        for (int i=0;i<=n;i++){
-            for (int j=0;j<2;j++){
-                dp[i][j][0]=0;
-            }
-        }
-        for (int i=0;i<2;i++){
-            for (int j=0;j<=limit;j++){
-                dp[n][i][j]=0;
-            }
-        }
+        vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(2,vector<int>(k+1,0)));
         for (int i=n-1;i>=0;i--){
-            for (int j=0;j<2;j++){
-                for (int k=1;k<limit+1;k++){
-                    if (j==1){
-                        dp[i][j][k]=max(dp[i+1][0][k]-prices[i], dp[i+1][j][k]);
+            for (int buy=0;buy<2;buy++){
+                for (int left=1;left<=k;left++){
+                    int profit=0;
+                    if (buy==0){
+                        profit=max(prices[i]+dp[i+1][1][left-1],dp[i+1][0][left]);
                     }else{
-                        dp[i][j][k]=max(dp[i+1][0][k],dp[i+1][1][k-1]+prices[i]);
+                        profit=max(-prices[i]+dp[i+1][0][left],dp[i+1][1][left]);
+
                     }
+                    dp[i][buy][left]=profit;
                 }
             }
         }
-
-        return dp[0][1][limit];
-    }   
+        return dp[0][1][k];
+    }
 };
