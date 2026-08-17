7b4d19e04176d2677b3d796fefeda7d94cdba6bc
akshat-bajpai <akshat24056@iiitd.ac.in>
2026-08-17 04:59:10 +0000

    [LeetCode Sync] Runtime - 1 ms (45.26%), Memory - 16.2 MB (53.26%)

--- problems/0309-best-time-to-buy-and-sell-stock-with-cooldown/solution.cpp
+++ problems/0309-best-time-to-buy-and-sell-stock-with-cooldown/solution.cpp
@@ -1,30 +1,37 @@
+/*
+f(i,buy) => max profit with buy status = buy (1 means can buy and dont hold anything, 0 means cannot buy and holding onto something) starting day i
+
+    if (i==n) return 0;
+
+    if (buy==1){
+        buy=-prices[i]+f(i+1,0)
+        dontbuy=f(i+1,1)
+    }else{
+        sell=prices[i];
+        if (i<n-1) sell+=f(i+2,1);
+        dontsell=f(i+1,0);
+    }
+*/
+
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-
-        vector<vector<int>> dp(n+1,vector<int>(2));
-
-        dp[n][0]=0;
-        dp[n][1]=0;
-
+        vector<vector<int>> dp(n+1,vector<int>(2,0));
         for (int i=n-1;i>=0;i--){
-            for (int buy=0;buy<=1;buy++){
-                if (buy){
-                    //if buy : f(ind+1,0)
-                    //if not buy : f(ind+1,1)
-                    dp[i][buy]=max(dp[i+1][0]-prices[i],dp[i+1][1]);
+            for (int j=0;j<2;j++){
+                if (j==1){
+                    int buy=-prices[i]+dp[i+1][0];
+                    int dontbuy=dp[i+1][1];
+                    dp[i][j]=max(buy,dontbuy);
                 }else{
-                    if (i<=n-2){
-                        dp[i][buy]=max(dp[i+2][1]+prices[i],dp[i+1][0]);
-                    }else{
-                        dp[i][buy]=max(dp[i+1][0],prices[i]);
-                    }
-
+                    int sell=prices[i];
+                    if (i<n-1) sell+=dp[i+2][1];
+                    int dontsell=dp[i+1][0];
+                    dp[i][j]=max(sell,dontsell);
                 }
             }
         }
-
         return dp[0][1];
     }
 };
