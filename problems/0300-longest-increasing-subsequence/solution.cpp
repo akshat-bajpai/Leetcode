902ca88c0d3d0546c0d34f84f4b7384ca4a60c47
akshat-bajpai <akshat24056@iiitd.ac.in>
2026-08-22 11:17:25 +0000

    [LeetCode Sync] Runtime - 531 ms (22.49%), Memory - 296.2 MB (30.12%)

--- problems/0300-longest-increasing-subsequence/solution.cpp
+++ problems/0300-longest-increasing-subsequence/solution.cpp
@@ -1,22 +1,14 @@
-/*
-f(i,j) => longest increasing subsequence starting at index i with last chose element as index j
-    if (i==n) return 0;
-    take=0;
-    if (nums[i]>nums[j]) take=1+f(i+1,i);
-    dont take=f(i+1,j);
-    return max(take,notTake)
-*/
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
         vector<vector<int>> dp(n+1,vector<int>(n+1,0));
         for (int i=n-1;i>=0;i--){
-            for (int j=-1;j<i;j++){
+            for (int j=-1;j<n;j++){
+                int notTake=dp[i+1][j+1];
                 int take=0;
-                if (j==-1 || nums[i]>nums[j]) take=1+dp[i+1][i+1];
-                int donttake=dp[i+1][j+1];
-                dp[i][j+1]=max(take,donttake);
+                if (j==-1 || nums[j]<nums[i] ) take=dp[i+1][i+1]+1;
+                dp[i][j+1]=max(take,notTake);
             }
         }
         return dp[0][0];
