7c921b8b313667bc7e0e7aedd8d882dd817b0f56
akshat-bajpai <akshat24056@iiitd.ac.in>
2026-09-11 21:44:53 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 14 MB (87.15%)

--- problems/0300-longest-increasing-subsequence/solution.cpp
+++ problems/0300-longest-increasing-subsequence/solution.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
-        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
-        for (int i=n-1;i>=0;i--){
-            for (int j=-1;j<n;j++){
-                int notTake=dp[i+1][j+1];
-                int take=0;
-                if (j==-1 || nums[j]<nums[i] ) take=dp[i+1][i+1]+1;
-                dp[i][j+1]=max(take,notTake);
+        vector<int> temp;
+        temp.push_back(nums[0]);
+        for (int i=1;i<n;i++){
+            if (nums[i]>temp.back()) temp.push_back(nums[i]);
+            else{
+                int ind=lower_bound(temp.begin(),temp.end(),nums[i])-temp.begin();
+                temp[ind]=nums[i];
             }
         }
-        return dp[0][0];
+        return temp.size();
     }
 };
